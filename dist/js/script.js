1a2de0cf1d6458e85a110fa18512918adc8fcecc
Adib Setiawan <itsmeadib.s@gmail.com>
2022-09-07 09:34:20 +0700

    adjusment scroll, add button to top, add behaviour in responsive navbar

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,19 +1,29 @@
 window.onscroll = () => {
-    const header = document.querySelector('header');
-    const fixedNav = header.offsetTop;
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+  const toTop = document.getElementById("to-top");
 
-    if(window.pageYOffset > fixedNav) {
-        header.classList.add('navbar-fixed')
-    } else {
-        header.classList.remove('navbar-fixed')
-    }
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
+  } else {
+    header.classList.remove("navbar-fixed");
+    toTop.classList.remove("flex");
+    toTop.classList.add("hidden");
+  }
+};
 
-}
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#navmenu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
 
-
-const hamburger = document.querySelector("#hamburger")
-const navMenu = document.querySelector('#navmenu')
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('hamburger-active')
-    navMenu.classList.toggle('hidden');
-})
+window.addEventListener("click", (e) => {
+  if (e.target != navMenu && e.target != hamburger) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
+});
